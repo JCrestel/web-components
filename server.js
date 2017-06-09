5261b293345df579ab62bf6b0cb1c95a0b980040
Jeremy Crestel <j.crestel@gmail.com>
2017-06-09 08:17:19 +0200

    Initialisation weather-info
    Composant de météo

---
+++ server.js
@@ -0,0 +1,24 @@
+// This is the default Glitch server.js to serve static things.
+
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+//app.use(express.static('public'));
+app.use(express.static(__dirname));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
