70d75f65e85a4f9492e03e4e59a407cb4a78e06b
Jérémy Crestel <j.crestel@gmail.com>
2017-06-09 10:50:50 +0200

    Update for polyfill

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("/", function (request, response) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
